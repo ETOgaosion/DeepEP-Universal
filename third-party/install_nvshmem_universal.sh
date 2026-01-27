5132d59c346e085f35a23f767d37a8ea32803745
Blue Space <gaoziyuan19@mails.ucas.ac.cn>
2026-01-27 13:40:38 +0800

    all work

--- third-party/install_nvshmem_universal.sh
+++ third-party/install_nvshmem_universal.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-cd third-party/nvshmem && \
-    mkdir -p build && \
-    cmake -DNVSHMEM_PREFIX=/usr/local/nvshmem -DCUDA_HOME=/usr/local/cuda -DNVSHMEM_USE_GDRCOPY=0 -S . -B build && \
-    cd build && \
-    make -j install
+wget https://developer.download.nvidia.com/compute/nvshmem/3.5.19/local_installers/nvshmem-local-repo-ubuntu2404-3.5.19_3.5.19-1_amd64.deb && \
+    dpkg -i nvshmem-local-repo-ubuntu2404-3.5.19_3.5.19-1_amd64.deb && \
+    sudo cp /var/nvshmem-local-repo-ubuntu2404-3.5.19/nvshmem-*-keyring.gpg /usr/share/keyrings/ && \
+    sudo apt-get update && \
+    sudo apt-get install -y nvshmem-cuda-12
 
-
+sudo mkdir /usr/local/nvshmem && \
+    sudo ln -s /usr/bin/nvshmem /usr/local/nvshmem/bin && \
+    sudo ln -s /usr/include/nvshmem /usr/local/nvshmem/include && \
+    sudo ln -s /usr/lib/x86_64-linux-gnu/nvshmem/12 /usr/local/nvshmem/lib
--- vibe-coding-workspace/codex/requirements/intra_node_cuda_p2p.md
+++ vibe-coding-workspace/codex/requirements/intra_node_cuda_p2p.md
@@ -5,5 +5,5 @@ TASK:
     - [ ] You should check whether current intra node implementation can work on blackwell, amphere and hopper machines
         - [ ]
     - [ ] disable the inter node build
-    - [ ] update readme for intra node only install methods, tell me how to install
-    - I will help you install and run intra_node test
+    - [ ] update setup.py and readme for intra node only install methods, tell me how to install
+    - I will help you install DeepEP and run intra_node test
